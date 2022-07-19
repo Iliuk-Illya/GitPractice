4deed772d8bc56ca7a3d86a35614c0133463eed7
Ільюк Ілля <i.ilyuk52@gmail.com>
2022-07-19 13:11:20 +0300

    add new files

--- first/first/main.cpp
+++ first/first/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+
+class Solution 
+{
+
+public:
+
+    int romanToInt(string s) 
+    {
+        for (int i = 0; i < s.Lenght; i++)
+        {
+            switch (s[i])
+            {
+                case ''
+                    break;
+                case ''
+                    break;
+                case ''
+                    break;
+                case ''
+                    break;
+                case ''
+                    break;
+                case ''
+                    break;
+                case ''
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+};
+
+
 int main()
 {
 	std::cout << "It`s so hard" << std::endl;
